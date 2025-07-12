fdf932572fc6c98f58885bed9d9ce2e7737da24d
BriCua <briant.cua.66@gmail.com>
2025-07-12 19:03:18 +0700

    fixed fatal error

--- src/pages/blog/BlogPostPage.jsx
+++ src/pages/blog/BlogPostPage.jsx
@@ -4,14 +4,6 @@ import { useParams } from 'react-router-dom';
 import sanityClient from '../../sanityClient';
 import { PortableText } from '@portabletext/react';
 import { formatDate } from '../../utils/date';
-import '../../partials/blog/post.css';
-
-const ptComponents = {
-  block: {
-    // Ex-level scope render
-    normal: ({children}) => <p className="portable-text-paragraph">{children}</p>,
-  },
-};
 
 export default function BlogPostPage() {
   const { slug } = useParams();
@@ -53,7 +45,7 @@ export default function BlogPostPage() {
         <p className="post-date">{formatDate(post.publishedAt)}</p>
         {post.mainImage && <img src={post.mainImage.asset.url} alt={post.title} />}
         <h5 className='post-excerpt'>{post.excerpt}</h5>
-        <PortableText value={post.body} components={ptComponents} />
+        <PortableText value={post.body} />
       </div>
     </>
   );
--- src/sanityClient.js
+++ src/sanityClient.js
@@ -2,7 +2,7 @@
 import {createClient} from '@sanity/client'
 
 export default createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // find this in sanity.json or manage.sanity.io
-  dataset: import.meta.env.VITE_SANITY_DATASET,
+  projectId: 'gpuveli0', // find this in sanity.json or manage.sanity.io
+  dataset: 'production',
   useCdn: true, // `false` if you want to ensure fresh data
 })
