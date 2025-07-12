8f2186c3c660f4169946d48550e8765d5fe224f3
BriCua <briant.cua.66@gmail.com>
2025-07-12 20:58:12 +0700

    testing purpose

--- src/pages/Karir.jsx
+++ src/pages/Karir.jsx
@@ -4,7 +4,7 @@ export default function Karir() {
       <title>Karir - PT. Century Bearindo International</title>
       <meta name="description" content="Bergabunglah dengan tim kami di PT. Century Bearindo International. Lihat lowongan kerja yang tersedia dan kirimkan aplikasi Anda."/>
       <meta name="keywords" content="karir, lowongan kerja, pekerjaan, bergabung dengan kami, rekrutmen" />
-      <div style={{padding:'2em'}}>Ini adalah halaman Karir.</div>
+      <div style={{padding:'2em'}}>Ini adalah halaman Karir. tester</div>
     </>
   );
 }
