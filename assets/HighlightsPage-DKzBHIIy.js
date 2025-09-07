67ceed47fe1255bb5ce8521948b9d246b96aaaf8
BriCua <briant.cua.66@gmail.com>
2025-09-07 12:52:43 +0700

    Updates

---
+++ assets/HighlightsPage-DKzBHIIy.js
@@ -0,0 +1,13 @@
+import{j as e}from"./animations-gUWowWhG.js";import{L as n,r as a}from"./vendor-ByeeqTfc.js";import{s as l,A as r}from"./index-Iyfm676k.js";import{f as c}from"./date-Bh8b8zhR.js";import"./sanity-D1Eq7Cx7.js";import"./icons-C4sLXFjC.js";function o({post:t}){return e.jsx("div",{className:"highlights-post-card",id:"break-ratio",children:e.jsxs(n,{to:`/highlights/${t.slug.current}`,children:[e.jsx("img",{src:t.mainImage.asset.url,alt:t.title}),e.jsxs("div",{className:"card-content",children:[e.jsx("h5",{children:t.title}),e.jsx("p",{className:"excerpt",children:t.excerpt}),e.jsx("p",{className:"post-date",children:c(t.publishedAt)})]})]})})}function x(){const[t,s]=a.useState([]);return a.useEffect(()=>{l.fetch(`*[_type == "post"]{
+          title,
+          slug,
+          publishedAt,
+          excerpt,
+          mainImage{
+            asset->{
+              _id,
+              url
+            }
+          },
+          "name": author->name,
+        }`).then(i=>s(i)).catch(console.error)},[]),e.jsxs(e.Fragment,{children:[e.jsx("title",{children:"Highlights - PT. Century Bearindo International"}),e.jsx("meta",{name:"description",content:"Baca artikel terbaru dari PT. Century Bearindo International mengenai industri, produk, dan layanan kami."}),e.jsx("meta",{name:"keywords",content:"Highlight, artikel, berita, informasi, industri, bearing"}),e.jsxs("div",{className:"mt-4 mb-16",children:[e.jsx(r,{distance:150,direction:"horizontal",reverse:!0,duration:1.5,ease:"power3.out",initialOpacity:0,animateOpacity:!0,scale:1,threshold:.2,delay:.5,children:e.jsxs("div",{className:"items-center flex-col group w-fit justify-self-center origin-center mb-12",children:[e.jsx("h1",{className:"def-ease-out  group-hover:tracking-[0.15em] ",children:"Highlights"}),e.jsx("hr",{className:"line def-ease-out group-hover:scale-x-500"})]})}),e.jsx(r,{distance:150,direction:"vertical",reverse:!1,duration:1.5,ease:"power3.out",initialOpacity:0,animateOpacity:!0,scale:1,threshold:.2,delay:.5,children:e.jsx("div",{className:"post-list flex justify-self-center justify-center flex-wrap gap-2",children:t.map(i=>e.jsx(o,{post:i},i.slug.current))})})]})]})}export{x as default};
---
+++ assets/HighlightsPostPage-CZDTrUGD.js
@@ -0,0 +1,14 @@
+import{j as t}from"./animations-gUWowWhG.js";import{e as r,r as a}from"./vendor-ByeeqTfc.js";import{s as i,A as m}from"./index-Iyfm676k.js";import{P as n}from"./sanity-D1Eq7Cx7.js";import{f as c}from"./date-Bh8b8zhR.js";import{S as x}from"./SkeletonLoader-CCrfOLFT.js";import"./icons-C4sLXFjC.js";const d={block:{h1:({children:e})=>t.jsx("h1",{className:"text-4xl font-bold my-4",children:e}),h2:({children:e})=>t.jsx("h2",{className:"text-3xl font-bold my-4",children:e}),h3:({children:e})=>t.jsx("h3",{className:"text-2xl font-bold my-4",children:e}),h4:({children:e})=>t.jsx("h4",{className:"text-xl font-bold my-4",children:e}),normal:({children:e})=>t.jsx("p",{className:"text-lg my-4 text-justify",children:e}),blockquote:({children:e})=>t.jsx("blockquote",{className:"border-l-4 border-gray-500 pl-4 my-4",children:e})},list:{bullet:({children:e})=>t.jsx("ul",{className:"list-disc list-inside my-4",children:e}),number:({children:e})=>t.jsx("ol",{className:"list-decimal list-inside my-4",children:e})}};function g(){const{slug:e}=r(),[s,l]=a.useState(null);return a.useEffect(()=>{i.fetch(`*[_type == "post" && slug.current == $slug][0]{
+          title,
+          slug,
+          publishedAt,
+          excerpt,
+          mainImage{
+            asset->{
+              _id,
+              url
+            }
+          },
+          body,
+          "name": author->name,
+        }`,{slug:e}).then(o=>l(o)).catch(console.error)},[e]),s?t.jsxs(t.Fragment,{children:[t.jsxs("title",{children:[s.title," - PT. Century Bearindo International"]}),t.jsx("meta",{name:"description",content:`Baca artikel terbaru dari PT. Century Bearindo International: ${s.title}`}),t.jsx(m,{distance:150,direction:"vertical",reverse:!1,duration:1.5,ease:"power3.out",initialOpacity:0,animateOpacity:!0,scale:1,threshold:.2,delay:.5,children:t.jsx("div",{className:"container mx-auto px-4 sm:px-6 lg:px-8 mt-4 mb-16",children:t.jsxs("div",{className:"max-w-3xl mx-auto",children:[t.jsx("h1",{className:"text-4xl font-bold text-left mb-4",children:s.title}),t.jsx("p",{className:"text-gray-500 text-sm mb-8",children:c(s.publishedAt)}),s.mainImage&&t.jsx("img",{src:s.mainImage.asset.url,alt:s.title,className:"rounded-2xl shadow-lg mb-8"}),t.jsx("h5",{className:"text-xl font-bold mb-4",children:s.excerpt}),t.jsx(n,{value:s.body,components:d})]})})})]}):t.jsx(x,{})}export{g as default};
