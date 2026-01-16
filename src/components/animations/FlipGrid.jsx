ee4289f4db876ee8856c23f2b9eab17d1342c343
BriCua <briant.cua.66@gmail.com>
2026-01-16 09:07:23 +0700

    cupdates and changes

--- src/components/animations/FlipGrid.jsx
+++ src/components/animations/FlipGrid.jsx
@@ -1,25 +1,24 @@
 import { useEffect, useMemo, useRef } from "react";
 
-
 const partners = [
-  { image: "./partners/partners-1.webp", link: "https://www.timken.com/products/timken-engineered-bearings/" },
-  { image: "./partners/partners-2.webp", link: "https://www.simatec.com/" },
-  { image: "./partners/partners-3.webp", link: "https://www.nachi-fujikoshi.co.jp/eng/" }, 
-  { image: "./partners/partners-4.webp", link: "https://www.nmbtc.com/" },
-  { image: "./partners/partners-5.webp", link: "https://www.schaeffler.com" },
-  { image: "./partners/partners-6.webp", link: "https://www.kawada.com.my/index.php?ws=showproducts&products_id=2479740" },
-  { image: "./partners/partners-7.webp", link: "https://www.nose-seiko.co.jp/en/" },
-  { image: "./partners/partners-8.webp", link: "https://www.schaeffler.com" },
-  { image: "./partners/partners-9.webp", link: "https://www.twb.com.cn/en/" }, 
-  { image: "./partners/partners-10.webp", link: "https://www.ijics.co.jp/en/" },
-  { image: "./partners/partners-11.webp", link: "https://www.fyh.co.jp/en/" },
-  { image: "./partners/partners-12.webp", link: "https://www.flexco.com/" }, // REMOVE
-  { image: "./partners/partners-13.webp", link: "https://hydraulictechnologies.com/brands/power-team" }, 
-  { image: "./partners/partners-14.webp", link: "https://www.fbj-bearings.com/" },
-  { image: "./partners/partners-15.webp", link: "https://www.schaeffler.com" },
-  { image: "./partners/partners-16.webp", link: "https://www.detmk.com/" },
-  { image: "./partners/partners-17.webp", link: "https://products.bandogrp.com/en/product/bancollan_bearing/" },
-  { image: "./partners/partners-18.webp", link: "https://global.abb/" }, // REMOVE
+  "./partners/partners-1.webp",
+  "./partners/partners-2.webp",
+  "./partners/partners-3.webp",
+  "./partners/partners-4.webp",
+  "./partners/partners-5.webp",
+  "./partners/partners-6.webp",
+  "./partners/partners-7.webp",
+  "./partners/partners-8.webp",
+  "./partners/partners-9.webp",
+  "./partners/partners-10.webp",
+  "./partners/partners-11.webp",
+  "./partners/partners-12.webp",
+  "./partners/partners-13.webp",
+  "./partners/partners-14.webp",
+  "./partners/partners-15.webp",
+  "./partners/partners-16.webp",
+  "./partners/partners-17.webp",
+  "./partners/partners-18.webp",
 ];
 
 export default function FlipGrid() {
@@ -34,7 +33,8 @@ export default function FlipGrid() {
       if (!card || !inner) return;
 
       // Set floating animation with random delay
-      const floatDelay = floatDelays[Math.floor(Math.random() * floatDelays.length)];
+      const floatDelay =
+        floatDelays[Math.floor(Math.random() * floatDelays.length)];
       card.style.animation = `float 3s ease-in-out infinite`;
       card.style.animationDelay = `${floatDelay}s`;
       card.style.animationPlayState = "running";
@@ -44,7 +44,8 @@ export default function FlipGrid() {
       const offset = Math.random() * 500; // one-time offset per card
       flipOffsets.current[i] = offset;
 
-      const getNextFlipTime = () => Date.now() + baseFlipTime + flipOffsets.current[i];
+      const getNextFlipTime = () =>
+        Date.now() + baseFlipTime + flipOffsets.current[i];
       let nextFlipTime = getNextFlipTime();
 
       const update = () => {
@@ -76,23 +77,18 @@ export default function FlipGrid() {
             className="absolute inset-0 transition-transform duration-700 [transform-style:preserve-3d] rounded-md shade"
           >
             <div className="absolute inset-0 front backface-hidden bg-white flex items-center justify-center rounded-md ">
-              <a href={partners[i].link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={partners[i].image}
-                  alt={`Partner ${i + 1}`}
+              <img
+                src={partners[i]}
+                alt={`Partner ${i + 1}`}
                 className="w-full h-full object-contain p-2 justify-self-center"
-
-                />
-              </a>
+              />
             </div>
             <div className="absolute inset-0 back backface-hidden bg-white [transform:rotateY(180deg)] flex items-center justify-center rounded-md">
-              <a href={partners[i + 9].link} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={partners[i + 9].image}
-                  alt={`Partner ${i + 10}`}
-                  className="w-full h-full object-contain p-2 justify-self-center"
-                />
-              </a>
+              <img
+                src={partners[i + 9]}
+                alt={`Partner ${i + 10}`}
+                className="w-full h-full object-contain p-2 justify-self-center"
+              />
             </div>
           </div>
         </div>
